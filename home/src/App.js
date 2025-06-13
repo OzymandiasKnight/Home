300aa3c9ba1e13101112eb75b90de87293f07464
BossEnderDrag <ozymandias.knight@gmail.com>
2025-06-13 21:32:50 +0200

    Adding Website Base

--- home/src/App.js
+++ home/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Route, Routes} from 'react-router-dom';
+import Skills from './pages/Skills';
+import Home from './pages/Home';
 
-function App() {
+
+
+const Main = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route exact path="/Home" element={<Home />}></Route>
+      <Route exact path="/about" element={<About />}></Route>
+      <Route exact path="/skills" element={<Skills />}></Route>
+    </Routes>
+  );
+};
+
+const Band = () => {
+  return (
+    <div className='band'>
+      <NavLink to={'/Home'}><h1>Accueil</h1></NavLink>
+      <NavLink to={'/skills'}><h1>Competences</h1></NavLink>
     </div>
   );
 }
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className='contacts'>
+        <h3>Téléphone : 06 20 69 48 89 &#40;Laissez moi un message&#41;</h3>
+        <h3>Email : patrick.nikielp@gmail.com</h3>
+        <a href="https://www.linkedin.com/in/patrick-nikiel/">Linkedin</a>
+        <a href="https://github.com/OzymandiasKnight/">GitHub</a>
+      </div>
+      <div className='infos'>
+        <h5>Site fait avec React</h5>
+        <h5>Site hébérgé gratuitement avec GitHub Pages</h5>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <Band />
+      <Main />
+      <About />
+    </div>
+  );
+};
 
 export default App;
