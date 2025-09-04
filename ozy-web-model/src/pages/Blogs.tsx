7c4b0dfe86262bec49d27a1b7cfdcc337b5d9b1a
OzymandiasKnight <ozymandias.knight@gmail.com>
2025-09-04 11:52:37 +0200

    Projects & Blogs Animations
    Adding Show Animations for Projects and Blogs

--- ozy-web-model/src/pages/Blogs.tsx
+++ ozy-web-model/src/pages/Blogs.tsx
@@ -1,20 +1,49 @@
 import Link from '../components/Link.tsx';
 
-function BlogsPage() {
+interface BlogInfos{
+    link:string,
+    title:string,
+}
+
+const blogs:BlogInfos[] = [
+    {link:"markdownsystem", title:"Mon système de blog personnalisé"},
+    {link:'cybersecurity', title:"Mes concours de cybersécurité"},
+    {link:'drawings', title:"Ma passion pour les dessins"}
+]
+
+function BlogLink(blog_infos:BlogInfos, index:number) {
+    const delay:string = String(index*0.1) + "s";
     const prefix:string = "/blogs/"
+    return (
+        <div style={{position:'relative',animationDelay:`${delay}`}} className="project-show">
+            <Link link={`${prefix}${blog_infos.link}`}>
+                <p>{blog_infos.title}</p>
+            </Link>
+        </div>
+    );
+}
+
+function BlogsList() {
+    return (
+        <>
+            {blogs.map((item, index) => {
+                return (
+                <div key={index}>
+                    {BlogLink(item,index)}
+                </div>
+                )
+
+            })}
+        </>
+    )
+}
+
+function BlogsPage() {
     return (
         <div style={{display:'flex', justifyContent:'center'}}>
             <div style={{width:'900px', minWidth:'300px'}}>
                 <h1>Mes Blogs :</h1>
-                <Link link={`${prefix}markdownsystem`}>
-                    <p>Mon système de blog personnalisé</p>
-                </Link>
-                <Link link={`${prefix}cybersecurity`}>
-                    <p>Mes concours de cybersécurité</p>
-                </Link>
-                <Link link={`${prefix}drawings`}>
-                    <p>Ma passion pour les dessins</p>
-                </Link>
+                {BlogsList()}
             </div>
         </div>
     );
--- ozy-web-model/src/pages/Projects.tsx
+++ ozy-web-model/src/pages/Projects.tsx
@@ -1,28 +1,60 @@
 import Grid from '../components/Grid.tsx';
 import Card from '../components/Card.tsx';
 
+interface ProjectInfos {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+}
+
+const projects: ProjectInfos[] = [
+    {title:"Guide Python", description:'Guide pour débuter en Python',
+    image:"showcases/GuidePython.png",link:"https://ozymandiasknight.github.io/GuidePython/guide_bases.pdf"},
+    {title:"Blood War", description:'Jeu de plateforme multijoueur local en 2D',
+    image:"showcases/Bloodwar.png",link:"https://github.com/OzymandiasKnight/BloodWar"},
+    {title:"Slower Pace", description:'Plugin Minecraft augmentant la durée du jeu',
+    image:"showcases/SlowerPace.png",link:"https://github.com/OzymandiasKnight/SlowerPace"},
+    {title:"Work Nodes", description:'Logiciel de gestion de projet',
+    image:"showcases/WorkNodes.png", link:"https://github.com/OzymandiasKnight/ProjectManager"},
+    {title:"Art Station", description:'Site vitrine de présentation de mes créations 3D',
+    image:"showcases/Artstation.png", link:"https://ozymandiasknight.github.io/Artstation/"},
+    {title:"Photo Builder",
+    description:'Mini réseaux social permettant de comparer les frises chronologiques de ses amis',
+    image:"showcases/PhotoBuilder.png", link:"https://github.com/OzymandiasKnight/SocialTimelineBuilder"},
+    {title:"Portfolio" , description:'Decouvrez le code de ce portfolio, lien GitHub',
+    image:"icons/logo.svg", link:"https://github.com/OzymandiasKnight/Home"}
+];
+
+function ProjectCard(infos:ProjectInfos,index:number) {
+    const delay:string = String(index*0.15) + "s";
+    return (
+        <div key={index} style={{position:'relative',animationDelay:`${delay}`}} className="project-show">
+            <Card title={infos.title} description={infos.description}
+            card_width="270px" card_height="300px"  image={infos.image} link={infos.link}/>
+        </div>
+    )
+}
+
+function ProjectsList(project_list:ProjectInfos[]) {
+    return (
+        <>
+        {project_list.map((item:ProjectInfos, index:number) => (
+            ProjectCard(item,index)
+        ))}
+        </>
+    )
+}
+
 function ProjectsPage() {
     return (
-        <div style={{}}>
+        <div>
             <div style={{width: '100%', display:'flex', justifyContent:'center', alignItems:'center',flexDirection:'column'}}>
                 <h1>Mes Projets</h1>
                 <div className="space-s"></div>
                 <div style={{minWidth:'320px', width:'100%', maxWidth:'900px', display: 'flex', justifyContent: 'center'}}>
                     <Grid size="270px">
-                        <Card title="Guide Python" description='Guide pour débuter en Python'
-                        card_width="270px" card_height="300px"  image="showcases/GuidePython.png" link="https://ozymandiasknight.github.io/GuidePython/guide_bases.pdf"/>
-                        <Card title="Blood War" description='Jeu de plateforme multijoueur local en 2D'
-                        card_width="270px" card_height="300px" image="showcases/Bloodwar.png" link="https://github.com/OzymandiasKnight/BloodWar"/>
-                        <Card title="Slower Pace" description='Plugin Minecraft augmentant la durée du jeu'
-                        card_width="270px" card_height="300px" image="showcases/SlowerPace.png" link="https://github.com/OzymandiasKnight/SlowerPace"/>
-                        <Card title="Work Nodes" description='Logiciel de gestion de projet'
-                        card_width="270px" card_height="300px" image="showcases/WorkNodes.png" link="https://github.com/OzymandiasKnight/ProjectManager"/>
-                        <Card title="Art Station" description='Site vitrine de présentation de mes créations 3D'
-                        card_width="270px" card_height="300px" image="showcases/Artstation.png" link="https://ozymandiasknight.github.io/Artstation/"/>
-                        <Card title="Photo Builder" description='Mini réseaux social permettant de comparer les frises chronologiques de ses amis'
-                        card_width="270px" card_height="300px" image="showcases/PhotoBuilder.png" link="https://github.com/OzymandiasKnight/SocialTimelineBuilder"/>
-                        <Card title="Portfolio" description='Decouvrez le code de ce portfolio, lien GitHub'
-                        card_width="270px" card_height="300px" image="icons/logo.svg" link="https://github.com/OzymandiasKnight/Home"/>
+                        {ProjectsList(projects)}
                     </Grid>
                 </div>
             </div>
